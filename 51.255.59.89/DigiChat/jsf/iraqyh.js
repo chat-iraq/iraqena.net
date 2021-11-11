7df551a1e00980062d7fc9f345a130ba80179000
chat-iraq <79407256+chat-iraq@users.noreply.github.com>
2021-11-11 19:50:58 +0300

    goo

---
+++ 51.255.59.89/DigiChat/jsf/iraqyh.js
@@ -0,0 +1,4 @@
+var ChatServer = '51.255.59.89';
+var ChatSiteID = 1003;
+var ChatPorts = '60413,60414,60415,60416';
+document.write('<scr' + 'ipt language="javascript" type="text/javascript" src="http://' + ChatServer + '/DigiChat/_.js?ts=' + (new Date).getTime() + '"></sc' + 'ript>');
